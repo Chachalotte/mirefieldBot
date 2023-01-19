86d29cc9943b76f94c760a80321f44720916def2
Charlotte Kindt <37578645+Chachalotte@users.noreply.github.com>
2023-01-19 21:11:26 +0100

    add dependencies

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
-
+const fs = require('fs');
+const path = require('path');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -13,7 +14,9 @@ client.once('ready', () => {
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -21,24 +24,26 @@ for (const file of commandFiles) {
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+	else {
+		console.log(
+			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+		);
 	}
 }
 
-
-
-
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
 
 	const { commandName } = interaction;
 
 	if (commandName === 'help') {
 		await interaction.reply('C\'est bien moi');
-	} else if (commandName === 'roll') {
+	}
+	else if (commandName === 'roll') {
 		await interaction.reply(Math.floor(Math.random() * 101).toString());
-	} else if (commandName === 'user') {
+	}
+	else if (commandName === 'user') {
 		await interaction.reply('User info.');
 	}
 });
